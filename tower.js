d025739c64615c61a2085f59db96739f0bce0485
jigisha-lal <72437022+jigisha-lal@users.noreply.github.com>
2020-12-08 20:45:34 +0530

    Add files via upload

---
+++ tower.js
@@ -0,0 +1,29 @@
+class Block{
+    constructor(x,y,width,height){
+        var option={
+            'restitution':0.04,
+            'friction':0.0,
+            
+        }
+        this.body=Bodies.rectangle(x,y,width,height,option)
+       this.width=width
+       this.height=height
+       World.add(world,this.body)
+    }
+   display(){
+       var pos=this.body.position
+       var angle=this.body.angle
+      push()
+      translate(pos.x,pos.y)
+      rotate(angle)
+      rectMode(CENTER)
+      strokeWeight(4)
+      stroke("red")
+      fill("yellow")
+      rect(0,0,this.width,this.height)
+      pop()
+
+   }
+   
+    
+}
